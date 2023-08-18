bc14fabece7ca597c235acd91e2499199caa6401
onnidahlman <142579293+onnidahlman@users.noreply.github.com>
2023-08-18 12:35:07 +0300

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,7 +1,6 @@
 git remote add upstream https://course-gitlab.tuni.fi/git-course/basics-materials.git
+
 git pull upstream main --allow-unrelated-histories
-git merge upstream/main
-git add .
-git commit -m "Lisää tekstin"
-git push upstream main
+
+git push origin main
 
