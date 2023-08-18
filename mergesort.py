6d00a568b4310dd3cc11b7ae1432925b07a32612
onnidahlman <142579293+onnidahlman@users.noreply.github.com>
2023-08-18 14:22:21 +0300

    Update and rename mergesort.py to mergesortti

--- mergesort.py
+++
@@ -1,8 +0,0 @@
-git config pull.rebase false
-git config pull.rebase true  
-git config pull.ff only
-
-git remote add upstream https://course-gitlab.tuni.fi/git-course/basics-materials.git
-git pull upstream main --allow-unrelated-histories
-git push origin main
-
